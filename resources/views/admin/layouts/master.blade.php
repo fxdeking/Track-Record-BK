f3aff1f4308fb666bd3fa27aa01690965258c122
fxdeking <hendytandika25@gmail.com>
2022-10-03 11:31:45 +0700

    buat file bagian dari template

---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,9 @@
+@include('admin.layouts.sidebar')
+@include('admin.layouts.navbar')
+<div class="container-fluid">
+    @include('admin.layouts.content')
+</div>
+<!-- /.container-fluid -->
+</div>
+<!-- End of Main Content -->
+@include('admin.layouts.footer')
