76a971f32cfe235c44fc087402ad880bc7a47112
keilamarie <keilastwn05@gmail.com>
2022-10-13 10:55:09 +0700

    sidebar

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -45,14 +45,16 @@
         <!-- Sign In Start -->
         <div class="container-fluid">
             <div class="row h-100 align-items-center justify-content-center" style="min-height: 100vh;">
-                <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
+                <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4" style="width:600px;">
                     <div class="bg-secondary rounded p-4 p-sm-5 my-4 mx-3">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
                         <div class="d-flex align-items-center justify-content-between mb-3">
+            
                             <a href="#" class="">
-                                <h3 class="text-primary"><i class="fa fa-user-edit me-2"></i>Track Record BK</h3>
+                                <h3 class="text-primary"><link href="{{ asset('gambar/trackrecordbklogo.jpeg') }}" rel="icon"> Track Record BK</h3>
                             </a>
+                            <br>
                             <h3>Sign In</h3>
                         </div>
                         <div class="form-floating mb-3">
