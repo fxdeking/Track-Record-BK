255d981042177bc79704b3f787441612c3bb9e7c
Azhreina <109132295+Azhreina@users.noreply.github.com>
2022-10-27 07:04:18 +0700

    dashboard

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -55,7 +55,7 @@
                         <div class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1"></div>
                     </div>
                     <div class="ms-3">
-                        <h6 class="mb-0">Jhon Doe</h6>
+                        <h6 class="mb-0">{{ Auth::user()->name }}</h6>
                         <span>Admin</span>
                     </div>
                 </div>
@@ -80,8 +80,8 @@
                         </div>
                     </div>
                     <a href="{{ asset('template/form.html') }}" class="nav-item nav-link"><i class="fa fa-keyboard me-2"></i>Jadwal Bertugas</a>
-                    <!-- <a href="widget.html" class="nav-item nav-link"><i class="fa fa-th me-2"></i>Widgets</a>
-                    <a href="table.html" class="nav-item nav-link"><i class="fa fa-laptop me-2"></i>Tables</a>
+                    <a href="#6" class="nav-item nav-link"><i class="fa fa-th me-2"></i>Catatan</a>
+                    <!-- <a href="table.html" class="nav-item nav-link"><i class="fa fa-laptop me-2"></i>Tables</a>
                     <a href="chart.html" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Charts</a> -->
                 
                 </div>
