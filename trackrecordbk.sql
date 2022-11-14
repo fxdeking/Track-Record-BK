e0472957861dd34f7fda0a56029f61c3d0c921d7
fxdeking <hendytandika25@gmail.com>
2022-11-14 10:29:21 +0700

    Update trackrecordbk.sql

--- trackrecordbk.sql
+++ trackrecordbk.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 10 Nov 2022 pada 07.13
+-- Waktu pembuatan: 14 Nov 2022 pada 10.28
 -- Versi server: 10.4.17-MariaDB
 -- Versi PHP: 7.4.27
 
@@ -29,18 +29,12 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `catatans` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `nama` varchar(35) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nama` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `catatan` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data untuk tabel `catatans`
---
-
-INSERT INTO `catatans` (`id`, `nama`, `catatan`, `created_at`, `updated_at`) VALUES
-(1, 'Hendy Tandika', 'testestes', '2022-11-09 17:04:57', '2022-11-09 17:04:57');
 
 -- --------------------------------------------------------
 
@@ -56,6 +50,68 @@ CREATE TABLE `failed_jobs` (
   `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jadwals`
+--
+
+CREATE TABLE `jadwals` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `hari` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jammulai` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jamselesai` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategoris`
+--
+
+CREATE TABLE `kategoris` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `deskripsi` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `konselors`
+--
+
+CREATE TABLE `konselors` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `jk` enum('Laki-laki','Perempuan') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `notelp` int(11) NOT NULL,
+  `jadwal_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `laporans`
+--
+
+CREATE TABLE `laporans` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `pengunjung_id` bigint(20) UNSIGNED NOT NULL,
+  `kategori_id` bigint(20) UNSIGNED NOT NULL,
+  `keluhan` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `kunjungan` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `keluar` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -78,7 +134,12 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
 (2, '2014_10_12_100000_create_password_resets_table', 1),
 (3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(4, '2022_10_27_003445_create_catatans_table', 1);
+(4, '2022_10_27_003445_create_catatans_table', 1),
+(5, '2022_11_11_035204_create_kategoris_table', 1),
+(6, '2022_11_11_042706_create_pengunjungs_table', 1),
+(7, '2022_11_11_042757_create_laporans_table', 1),
+(8, '2022_11_11_042816_create_jadwals_table', 1),
+(9, '2022_11_11_223535_create_konselors_table', 1);
 
 -- --------------------------------------------------------
 
@@ -90,6 +151,25 @@ CREATE TABLE `password_resets` (
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengunjungs`
+--
+
+CREATE TABLE `pengunjungs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `kategori_id` bigint(20) UNSIGNED NOT NULL,
+  `nama` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `kelas` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `usia` int(11) NOT NULL,
+  `jk` enum('Laki-laki','Perempuan') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `alamat` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `notelp` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -112,13 +192,6 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data untuk tabel `users`
---
-
-INSERT INTO `users` (`id`, `name`, `foto`, `telepon`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Hendy Tandika', 'default.png', '-', 'hendytandika25@gmail.com', NULL, '$2y$10$yjm.M0X9qLd8cvDbHeR2leozpDR65pmbUzvasrHypZzd9QIMI1Phi', NULL, '2022-11-09 08:13:12', '2022-11-09 08:13:12');
-
---
 -- Indexes for dumped tables
 --
 
@@ -126,7 +199,8 @@ INSERT INTO `users` (`id`, `name`, `foto`, `telepon`, `email`, `email_verified_a
 -- Indeks untuk tabel `catatans`
 --
 ALTER TABLE `catatans`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `catatans_user_id_foreign` (`user_id`);
 
 --
 -- Indeks untuk tabel `failed_jobs`
@@ -134,6 +208,33 @@ ALTER TABLE `catatans`
 ALTER TABLE `failed_jobs`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indeks untuk tabel `jadwals`
+--
+ALTER TABLE `jadwals`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `kategoris`
+--
+ALTER TABLE `kategoris`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `konselors`
+--
+ALTER TABLE `konselors`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `konselors_jadwal_id_foreign` (`jadwal_id`);
+
+--
+-- Indeks untuk tabel `laporans`
+--
+ALTER TABLE `laporans`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `laporans_pengunjung_id_foreign` (`pengunjung_id`),
+  ADD KEY `laporans_kategori_id_foreign` (`kategori_id`);
 
 --
 -- Indeks untuk tabel `migrations`
@@ -146,6 +247,13 @@ ALTER TABLE `migrations`
 --
 ALTER TABLE `password_resets`
   ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indeks untuk tabel `pengunjungs`
+--
+ALTER TABLE `pengunjungs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `pengunjungs_kategori_id_foreign` (`kategori_id`);
 
 --
 -- Indeks untuk tabel `users`
@@ -162,7 +270,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT untuk tabel `catatans`
 --
 ALTER TABLE `catatans`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `failed_jobs`
@@ -171,16 +279,75 @@ ALTER TABLE `failed_jobs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT untuk tabel `jadwals`
+--
+ALTER TABLE `jadwals`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `kategoris`
+--
+ALTER TABLE `kategoris`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `konselors`
+--
+ALTER TABLE `konselors`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `laporans`
+--
+ALTER TABLE `laporans`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT untuk tabel `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT untuk tabel `pengunjungs`
+--
+ALTER TABLE `pengunjungs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `catatans`
+--
+ALTER TABLE `catatans`
+  ADD CONSTRAINT `catatans_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `konselors`
+--
+ALTER TABLE `konselors`
+  ADD CONSTRAINT `konselors_jadwal_id_foreign` FOREIGN KEY (`jadwal_id`) REFERENCES `jadwals` (`id`) ON DELETE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `laporans`
+--
+ALTER TABLE `laporans`
+  ADD CONSTRAINT `laporans_kategori_id_foreign` FOREIGN KEY (`kategori_id`) REFERENCES `kategoris` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `laporans_pengunjung_id_foreign` FOREIGN KEY (`pengunjung_id`) REFERENCES `pengunjungs` (`id`) ON DELETE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `pengunjungs`
+--
+ALTER TABLE `pengunjungs`
+  ADD CONSTRAINT `pengunjungs_kategori_id_foreign` FOREIGN KEY (`kategori_id`) REFERENCES `kategoris` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
