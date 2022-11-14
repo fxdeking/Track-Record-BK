e901f68bf421adf67c2dc6b20b275a33264ed68a
fxdeking <hendytandika25@gmail.com>
2022-11-14 11:13:45 +0700

    Update index.blade.php

--- resources/views/aboutus/index.blade.php
+++ resources/views/aboutus/index.blade.php
@@ -9,7 +9,7 @@
         </div>
         <br><br>
         <div class="d-flex align-items-center justify-content-between mb-4">
-            Kami adalah kelompok ... kelas XII RPL 1 Cinta kasih Tzu Chi angkatan 2022/2023 yang beranggotakan ... website ini kami buat sebagai rekam jejak untuk bimbingan konseling agar dapat mempermudah dalam pengecekan segala hal yang telah dilakukan oleh BK sehingga dapat menunjukkan kualitas dan kuantitas BK dalam membantu mengatasi masalah, dan lain sebagainya. Harapannya dengan adanya track record ini dapat membantu para konselor atau guru BK dalam mendata laporan konseling secara akurat.
+            Kami adalah kelompok dari kelas XII RPL 1 Cinta kasih Tzu Chi angkatan 2022/2023 yang beranggotakan Hendy Tandika, Keila Marie Setiawan, Cecilya Lim, Steaven, Leonardo Setiawan, dan William. Website ini kami buat sebagai rekam jejak untuk bimbingan konseling agar dapat mempermudah dalam pengecekan segala hal yang telah dilakukan oleh BK sehingga dapat menunjukkan kualitas dan kuantitas BK dalam membantu mengatasi masalah, dan lain sebagainya. Harapannya dengan adanya track record ini dapat membantu para konselor atau guru BK dalam mendata laporan konseling secara akurat.
         </div>
     </div>
 </div>
